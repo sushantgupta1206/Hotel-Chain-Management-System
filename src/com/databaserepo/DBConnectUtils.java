224c6ace5f07acdd96daad0245dc122277dcd63b
kshah3 <kshah3@lenovo.com>
2018-04-14 19:37:57 -0400

    Changed DButils property

--- src/com/databaserepo/DBConnectUtils.java
+++ src/com/databaserepo/DBConnectUtils.java
@@ -13,26 +13,19 @@ import java.util.logging.Logger;
 public class DBConnectUtils {
 	private static String sourceClass = DBConnectUtils.class.getName();
 	private static Logger log = Logger.getLogger(sourceClass);
-	public static final String DRIVER_NAME_DB2 = "com.ibm.db2.jcc.DB2Driver";
-	public static final String DATABASE_HOSTNAME_DB2 = "mtdb01.multitool.lenovo.com";
-	public static final String DATABASE_PORTNUMBER_DB2 = "50002";
-	public static final String DATABASE_NAME_DB2 = "Multidb";
-	public static final String DATABASE_USERNAME_DB2 = "custlogs";
-	public static final String DATABASE_PASSWORD_DB2 = "zaq12wsx";
 	
 	private static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/ljackma"; // Using SERVICE_NAME
     private static final String user = "ljackma";
     private static final String password = "group4";    
 
 
-	public static String DBSCHEMA="db2inst2";
-    //public static String DBSCHEMA="ljackma";
+    public static String DBSCHEMA="ljackma";
 	public final Connection getConnection(int flag) throws SQLException {
 		String sourceMethod = "getConnection";
 
-		if (flag == 1) {
-			// If DB2 connection
-			DBSCHEMA="db2inst2";
+		if (flag == -12) {
+			// If DB2 connection practice version
+			DBSCHEMA="ljackma";
 			Connection dbConn = getDB2Connection();
 			log.exiting(sourceClass, sourceMethod);
 			return dbConn;
@@ -59,13 +52,7 @@ public class DBConnectUtils {
 	}
 
 	private Connection getDB2Connection() throws SQLException {
-		try {
-			Class.forName(DRIVER_NAME_DB2);
-		} catch (ClassNotFoundException thisIsBad) {
-			throw new IllegalArgumentException(thisIsBad);
-		}
-		String url = "jdbc:db2://" + DATABASE_HOSTNAME_DB2 + ":" + DATABASE_PORTNUMBER_DB2 + "/" + DATABASE_NAME_DB2;
-		Connection db2Conn = DriverManager.getConnection(url, DATABASE_USERNAME_DB2, DATABASE_PASSWORD_DB2);
-		return db2Conn;
+		//Replaced all Maria db connection with db2.
+		return getMariaDBConnection();
 	}
 }
